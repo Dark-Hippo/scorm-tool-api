7c84b4585f5a7c6438100b26ae9265970e631af9
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-03-15 17:46:40 +0000

    Add skeleton of validate endpoint

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fileUpload, { FileArray } from 'express-fileupload';
 
 dotenv.config();
 
@@ -8,9 +9,29 @@ const server: Express = express();
 const port = process.env.PORT || 3001;
 
 server.use(cors());
+server.use(fileUpload());
 
 server.get('/health', (req: Request, res: Response) => {
   res.send({ date: new Date() });
+});
+
+server.post('/scorm/validate', async (req: Request, res: Response) => {
+  try {
+    if (!req.files) {
+      res.status(400).send({ message: 'No file sent' });
+    }
+
+    const files = req.files;
+    console.log(files);
+
+    // Unzip to system temp dir
+    // Check for 'scormcontent' directory
+    // Check for index.html file inside directory
+
+    res.status(200).send({ isValid: true });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 server.listen(port, () => {
