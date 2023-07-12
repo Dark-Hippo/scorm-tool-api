37d2a19e3f1c0c817738a7b5ade743b778a98896
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-07-12 18:31:40 +0100

    Updated auth function

--- src/middleware/auth0.ts
+++ src/middleware/auth0.ts
@@ -1,6 +1,7 @@
 import { auth } from 'express-oauth2-jwt-bearer';
 
 export const validateAccessToken = auth({
-  audience: process.env.AUTH_AUDIENCE,
-  issuerBaseURL: process.env.AUTH_BASE_URL,
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_BASE_URL,
+  tokenSigningAlg: 'RS256',
 });
