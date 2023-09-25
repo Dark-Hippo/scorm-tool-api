cb13749778b9f8c0a8e9068eb7a1fd00d80075fa
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-09-25 11:05:53 +0100

    Add delete user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response, Router } from 'express';
 import { User } from '@prisma/client';
 import { logError } from '../utils/logger';
-import { createUser, getAllUsers, getUser, updateUser } from '../adapters/user';
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from '../adapters/user';
 
 const router: Router = express.Router();
 
@@ -77,6 +83,41 @@ router.patch('/:id?', async (req: Request, res: Response) => {
   } catch (error) {
     logError(error);
     return res.status(500).send(error);
+  }
+});
+
+router.delete('/:id?', async (req: Request, res: Response) => {
+  try {
+    if (!req.params?.id) {
+      return res
+        .status(400)
+        .send({ message: 'An id is required to delete', isValid: false });
+    }
+
+    if (!Number.isInteger(Number(req.params.id))) {
+      return res.status(400).send({
+        message: `User Id must be a number, not '${req.params.id}'`,
+        isValid: false,
+      });
+    }
+
+    const id: number = Number(req.params.id);
+
+    const user = await getUser(id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .send({ message: `UserId ${id} not found`, isValid: false });
+    }
+
+    await deleteUser(id);
+
+    return res
+      .status(200)
+      .send({ message: `UserId ${id} deleted`, isValid: true });
+  } catch (error) {
+    logError(error);
   }
 });
 
