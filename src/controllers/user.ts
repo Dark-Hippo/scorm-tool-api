18cb83c1edfefd6b0418746be9d8a85f7fbeccac
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-09-26 16:38:31 +0100

    Add token validation to user endpoints

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,38 +8,43 @@ import {
   getUser,
   updateUser,
 } from '../adapters/user';
+import { validateAccessToken } from '../middleware/auth0';
 
 const router: Router = express.Router();
 
-router.get('/:id?', async (req: Request, res: Response) => {
-  try {
-    if (req.params?.id && !Number.isInteger(Number(req.params.id))) {
-      return res
-        .status(400)
-        .send({ message: 'Id must be a number', isValid: false });
-    }
-
-    if (req.params.id) {
-      const id: number = Number(req.params.id);
-      const user = await getUser(id);
-      if (!user) {
+router.get(
+  '/:id?',
+  validateAccessToken,
+  async (req: Request, res: Response) => {
+    try {
+      if (req.params?.id && !Number.isInteger(Number(req.params.id))) {
         return res
-          .status(404)
-          .send({ message: 'User not found', isValid: false });
+          .status(400)
+          .send({ message: 'Id must be a number', isValid: false });
       }
 
-      return res.status(200).send(user);
-    } else {
-      const users = await getAllUsers();
-      return res.status(200).send(users);
-    }
-  } catch (error) {
-    logError(error);
-    return res.status(500).send(error);
-  }
-});
+      if (req.params.id) {
+        const id: number = Number(req.params.id);
+        const user = await getUser(id);
+        if (!user) {
+          return res
+            .status(404)
+            .send({ message: 'User not found', isValid: false });
+        }
 
-router.post('/', async (req: Request, res: Response) => {
+        return res.status(200).send(user);
+      } else {
+        const users = await getAllUsers();
+        return res.status(200).send(users);
+      }
+    } catch (error) {
+      logError(error);
+      return res.status(500).send(error);
+    }
+  }
+);
+
+router.post('/', validateAccessToken, async (req: Request, res: Response) => {
   try {
     const user: User = req.body as User;
     const newUser = await createUser(user);
@@ -51,74 +56,82 @@ router.post('/', async (req: Request, res: Response) => {
   }
 });
 
-router.put('/:id?', async (req: Request, res: Response) => {
-  try {
-    if (!req.params?.id) {
-      return res
-        .status(400)
-        .send({ message: 'An id is required to update', isValid: false });
+router.put(
+  '/:id?',
+  validateAccessToken,
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.params?.id) {
+        return res
+          .status(400)
+          .send({ message: 'An id is required to update', isValid: false });
+      }
+
+      if (!Number.isInteger(Number(req.params.id))) {
+        return res.status(400).send({
+          message: `User Id must be a number, not '${req.params.id}'`,
+          isValid: false,
+        });
+      }
+
+      const id: number = Number(req.params.id);
+      const userData: User = req.body;
+
+      const user = await getUser(id);
+
+      if (!user) {
+        return res
+          .status(404)
+          .send({ message: `UserId ${id} not found`, isValid: false });
+      }
+
+      const updatedUser = await updateUser(userData, user.id);
+
+      return res.status(200).send(updatedUser);
+    } catch (error) {
+      logError(error);
+      return res.status(500).send(error);
     }
-
-    if (!Number.isInteger(Number(req.params.id))) {
-      return res.status(400).send({
-        message: `User Id must be a number, not '${req.params.id}'`,
-        isValid: false,
-      });
-    }
-
-    const id: number = Number(req.params.id);
-    const userData: User = req.body;
-
-    const user = await getUser(id);
-
-    if (!user) {
-      return res
-        .status(404)
-        .send({ message: `UserId ${id} not found`, isValid: false });
-    }
-
-    const updatedUser = await updateUser(userData, user.id);
-
-    return res.status(200).send(updatedUser);
-  } catch (error) {
-    logError(error);
-    return res.status(500).send(error);
   }
-});
+);
 
-router.delete('/:id?', async (req: Request, res: Response) => {
-  try {
-    if (!req.params?.id) {
+router.delete(
+  '/:id?',
+  validateAccessToken,
+  async (req: Request, res: Response) => {
+    try {
+      if (!req.params?.id) {
+        return res
+          .status(400)
+          .send({ message: 'An id is required to delete', isValid: false });
+      }
+
+      if (!Number.isInteger(Number(req.params.id))) {
+        return res.status(400).send({
+          message: `User Id must be a number, not '${req.params.id}'`,
+          isValid: false,
+        });
+      }
+
+      const id: number = Number(req.params.id);
+
+      const user = await getUser(id);
+
+      if (!user) {
+        return res
+          .status(404)
+          .send({ message: `UserId ${id} not found`, isValid: false });
+      }
+
+      await deleteUser(id);
+
       return res
-        .status(400)
-        .send({ message: 'An id is required to delete', isValid: false });
+        .status(200)
+        .send({ message: `UserId ${id} deleted`, isValid: true });
+    } catch (error) {
+      logError(error);
     }
-
-    if (!Number.isInteger(Number(req.params.id))) {
-      return res.status(400).send({
-        message: `User Id must be a number, not '${req.params.id}'`,
-        isValid: false,
-      });
-    }
-
-    const id: number = Number(req.params.id);
-
-    const user = await getUser(id);
-
-    if (!user) {
-      return res
-        .status(404)
-        .send({ message: `UserId ${id} not found`, isValid: false });
-    }
-
-    await deleteUser(id);
-
-    return res
-      .status(200)
-      .send({ message: `UserId ${id} deleted`, isValid: true });
-  } catch (error) {
-    logError(error);
   }
-});
+);
 
 export default router;
