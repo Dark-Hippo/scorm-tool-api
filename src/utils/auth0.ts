9b4e2d0a6eb12dab4568616a3330c15d687e1719
Tom Odell <Dark-Hippo@users.noreply.github.com>
2023-10-03 16:13:17 +0100

    Add get user call to auth0

--- src/utils/auth0.ts
+++ src/utils/auth0.ts
@@ -2,6 +2,42 @@ import { User } from '@prisma/client';
 import { log, logError } from './logger';
 import { generate } from 'generate-password';
 
+export const getUser = async (authId: string): Promise<User | null> => {
+  try {
+    const auth0BaseUrl = process.env.AUTH0_BASE_URL;
+    const auth0ApiToken = process.env.AUTH0_MANAGEMENT_API_TOKEN;
+
+    if (!auth0BaseUrl) throw new Error('AUTH0_BASE_URL is not defined');
+    if (!auth0ApiToken)
+      throw new Error('AUTH0_MANAGEMENT_API_TOKEN is not defined');
+
+    const response = await fetch(`${auth0BaseUrl}api/v2/users/${authId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth0ApiToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      const errorBody = await response.json();
+      throw new Error(`Failed to get user: ${errorBody.message}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    if (error instanceof TypeError) {
+      logError(`Network error: ${error.message}`);
+    } else if (error instanceof SyntaxError) {
+      logError(`Invalid response body: ${error.message}`);
+    } else {
+      logError(error);
+    }
+    throw error;
+  }
+};
+
 export const createUser = async (user: User): Promise<string> => {
   try {
     const auth0BaseUrl = process.env.AUTH0_BASE_URL;
